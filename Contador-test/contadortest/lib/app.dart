388cd15d03565e3ad95358c2a4b96d7e12fe5cb8
EMEBE <81788252+MilthonB@users.noreply.github.com>
2024-05-18 13:26:37 -0600

    Test another branch no importante

---
+++ Contador-test/contadortest/lib/app.dart
@@ -0,0 +1,17 @@
+
+import 'package:contadortest/src/pages/contadorPage.dart';
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Contadorpage(),
+      debugShowCheckedModeBanner: false,
+    );
+  } 
+  
+
+}
